9cc81f20d0a62d15670ad9f3c87a8df8677b9bf2
Petrav <vagi.p94@gmail.com>
2019-01-23 21:39:42 +0100

    EDIT_ANSWER.HTML, DATA_MANAGER.PY, SERVER.PY: New function for editing answers

--- server.py
+++ server.py
@@ -39,7 +39,6 @@ def get_question_details(question_id):
     if len(answers) > 0:
         answers_header = [header.replace("_", " ").capitalize() for header in answers[0].keys()]
     else:
-        answers = [{'TEST': 'There are no answers here'}]
         answers_header = []
 
     return render_template('question_details.html',
@@ -86,7 +85,7 @@ def add_answer(question_id):
     :param question_id: (string)- the ID of the selected question
     """
     if request.method == 'GET':
-        return render_template('add_answer.html')
+        return render_template('add_answer.html', question_id=question_id)
 
     if request.method == 'POST':
 
@@ -99,6 +98,37 @@ def add_answer(question_id):
 
         # Writing the new answer to the database
         data_manager.add_answer(new_answer)
+
+        # Generating the URL for the question which the answer belongs to
+        question_url = url_for('get_question_details', question_id=question_id)
+
+        return redirect(question_url)
+
+
+@app.route('/answer/<answer_id>/edit', methods=['GET', 'POST'])
+def edit_answer(answer_id):
+    """
+    This function directs to a page with a form where we can edit an existing answer,
+    after that it updates the database and redirects to the question_details page.
+    :param answer_id: (string)- the ID of the selected answer
+    """
+    if request.method == 'GET':
+
+        # Reading the original answer from the database
+        answer_message = data_manager.get_answer_message_by_id(answer_id)[0]['message']
+        return render_template('edit_answer.html', answer_id=answer_id, answer_message=answer_message)
+
+    if request.method == 'POST':
+
+        # Generating the new dictionary for the updated answer
+        updated_answer = {
+            'id': int(answer_id),
+            'submission_time': datetime.now(),
+            'message': request.form.get('answer')
+        }
+
+        # Updating the database and getting back the id of the question which the answer belongs to
+        question_id = str(data_manager.update_answer(updated_answer)[0]['question_id'])
 
         # Generating the URL for the question which the answer belongs to
         question_url = url_for('get_question_details', question_id=question_id)
